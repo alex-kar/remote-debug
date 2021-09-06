b46d5a09068961fa03da7f84ba9383fc0567e28b
Alex Karpov <alexk@sqreamtech.com>
2021-09-06 11:05:36 +0300

    init

---
+++ src/main/java/io/github/alexkar/remotedebug/RemoteDebugApplication.java
@@ -0,0 +1,13 @@
+package io.github.alexkar.remotedebug;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemoteDebugApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RemoteDebugApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/alexkar/remotedebug/RemoteDebugApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.alexkar.remotedebug;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemoteDebugApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
